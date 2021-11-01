b3d5fb99363e7b86d240fb995b63c5e7ed9c12fe
luthfiyahnra <luthfiyah.nur.arofah-2020@vokasi.unair.ac.id>
2021-11-01 21:30:35 +0700

    COBA

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -6,7 +6,7 @@
 <!-- ======= Hero Section ======= -->
   <section id="hero" class="d-flex justify-content-center align-items-center">
     <div class="container position-relative" data-aos="zoom-in" data-aos-delay="100">
-      <h1>Learning Today,<br>Leading Tomorrow</h1>
+      <h1>TPQ TOP,<br>Leading Tomorrow</h1>
       <h2>We are team of talented designers making websites with Bootstrap</h2>
       <a href="courses.html" class="btn-get-started">Get Started</a>
     </div>
